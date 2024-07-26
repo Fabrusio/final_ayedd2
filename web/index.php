bd31adf42553711de16169293c58d9cc93c3f0d0
Fabrusio <fabrusio3p3@gmail.com>
2024-07-26 04:50:20 -0300

    empezando con la vista, estilos y archivos js entre otros para el login

--- web/index.php
+++ web/index.php
@@ -1,8 +1,20 @@
 <?php
-echo 'probado ';
-
 require 'config/MysqlDb.php';
+// echo 'probado ';
 
-model_sql::testConnection();
+spl_autoload_register(function ($class) {
+	if (strpos($class, "Controller")) {
+		require_once 'controllers/' . $class . '.php';
+	}
+	if (strpos($class, "Model")) {
+		require_once 'models/' . $class . '.php';
+	};
+});
+
+$index = new IndexController;
+$index->run();
+
+
+// model_sql::testConnection();
 
 ?>
