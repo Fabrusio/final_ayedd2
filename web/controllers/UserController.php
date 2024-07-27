639ad5e3203498ff23e1d4e422f03a3209277c1e
Fabrusio <fabrusio3p3@gmail.com>
2024-07-27 04:13:56 -0300

    modelo y controlador de usuarios para realizar el login

---
+++ web/controllers/UserController.php
@@ -0,0 +1,59 @@
+<?php
+
+class UserController{
+
+    public function control_login()
+    {
+        if (!empty($_POST['mail']) && !empty($_POST['password'])) {
+            $mail = $_POST['mail'];
+            $password = $_POST['password'];
+            $verificar = UserModel::login($mail, $password);
+
+            if ($verificar != false) {
+                $mail_user = $verificar['email'];
+                $id_user = $verificar['id_user'];
+                $rol = $verificar['fk_rol_id'];
+                $state = $verificar['state'];
+                $changed = $verificar['change_password'];
+    
+                if ($state == 1) {
+                    $_SESSION['state'] = $state;
+                    $_SESSION['email'] = $mail_user;
+                    $_SESSION['fk_rol_id'] = $rol;
+                    $_SESSION['id_user'] = $id_user;
+                    $_SESSION['change_password'] = $changed;
+    
+    
+                    echo '<script>
+                    if (window.history.replaceState) {
+                        window.history.replaceState(null, null, window.location.href);
+                    }
+                    window.location="../index.php?pages=home";
+                    </script>';
+                }
+            } else {
+                echo '<script>
+                if (window.history.replaceState) {
+                    window.history.replaceState(null, null, window.location.href);
+                }
+                </script>
+                <div class="alert alert-danger mt-2">Usuario o contraseña incorrecta</div>';
+            }
+        } else {
+            echo '<script>
+            if (window.history.replaceState) {
+                window.history.replaceState(null, null, window.location.href);
+            }
+            alert("Debes completar los campos");
+            </script>';
+        }
+    }
+    
+
+
+
+
+}
+
+
+?>
